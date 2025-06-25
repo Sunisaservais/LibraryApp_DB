9a893c8b6c7540f774225da3727a9e0c4f2b2ccc
sunisaservais <sunisaservais@gmaill.com>
2025-06-25 17:04:09 -0400

    Login feature and StepDefinition added

--- src/test/resources/queries/library.sql
+++ src/test/resources/queries/library.sql
@@ -1,9 +1,16 @@
-SELECT COUNT(*) FROM books;
+SELECT COUNT(*)
+FROM books;
 
-SELECT COUNT(*) FROM users;
+SELECT COUNT(*)
+FROM users;
 
-SELECT COUNT(*) FROM book_borrow
+SELECT COUNT(*)
+FROM book_borrow
 WHERE is_returned = 0;
 
-SELECT name FROM book_categories;
+SELECT name
+FROM book_categories;
 
+SELECT name,author,isbn,year,description
+FROM books
+WHERE name = 'Agile Testing';
